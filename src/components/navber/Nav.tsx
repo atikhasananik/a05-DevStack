378226887fc5ca0be8841043f86a01179f6d629e
Atik Hasan Anik <itsatikhasananik@gmail.com>
2026-09-11 10:28:17 +0600

    feature/navber section done and it's work well

---
+++ src/components/navber/Nav.tsx
@@ -0,0 +1,33 @@
+import logo from "../../assets/logo-text.png";
+import "../../index.css";
+
+const Nav = () => {
+  return (
+    <div className="bg-[#ffffffde] py-5 sticky top-0 border-b-gray-200  border hover:shadow">
+      <div className="flex justify-between items-center gap-15 container mx-auto ">
+        <div className="">
+          <img src={logo} alt="DevStack Logo here." className="w-full h-full" />
+        </div>
+
+        <ul className="flex items-center gap-8 font-light text-[#475569]">
+          <li style={{color:"var(--nav-btn-color)" }}className="text-lg font-semibold ">
+            Home
+          </li>
+          <li className="text-lg ">Technologies</li>
+          <li className="text-lg ">Projects</li>
+          <li className="text-lg ">About</li>
+          <li className="text-lg ">Contact</li>
+        </ul>
+
+        <div className="flex items-center gap-5">
+          <button>Sing In</button>
+          <button style={{backgroundColor:"var(--nav-btn-color)"}} className="py-2.5 px-5 rounded-4xl  text-white ">
+            Sing Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
